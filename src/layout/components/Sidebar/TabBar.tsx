8c66b128c6b194154b2e8cea51f9cd8f677512a0
ujjwal1103 <ujjwallade11@gmail.com>
2024-07-27 17:31:59 +0530

    vercel.json added

--- src/layout/components/Sidebar/TabBar.tsx
+++ src/layout/components/Sidebar/TabBar.tsx
@@ -1,56 +1,56 @@
-import { cn } from "@/lib/utils";
-import { useModalStateSlice } from "@/redux/services/modalStateSlice";
-import { motion } from "framer-motion";
-import { Home, SearchIcon, SquarePlus, Heart, User2 } from "lucide-react";
-import { NavLink } from "react-router-dom";
+import { cn } from '@/lib/utils'
+import { useModalStateSlice } from '@/redux/services/modalStateSlice'
+import { motion } from 'framer-motion'
+import { Home, SearchIcon, SquarePlus, Heart, User2 } from 'lucide-react'
+import { NavLink } from 'react-router-dom'
 
 const sidebarRoutes: any[] = [
-  { route: "/", label: "Home", icon: <Home /> },
+  { route: '/', label: 'Home', icon: <Home /> },
   {
-    route: "/search",
-    label: "Search",
+    route: '/search',
+    label: 'Search',
     icon: <SearchIcon />,
-    modalName: "searchSheet",
+    modalName: 'searchSheet',
   },
   // { route: "/explore", label: "Explore", icon: <Compass /> },
   // { route: "/reels", label: "Reels", icon: <SquarePlay /> },
   // { route: "/inbox", label: "Messages", icon: <Send /> },
 
   {
-    route: "/create",
-    label: "Create",
+    route: '/create',
+    label: 'Create',
     icon: <SquarePlus />,
     modal: true,
-    modalName: "openPostModal",
+    modalName: 'openPostModal',
   },
   {
-    route: "/notifications",
-    label: "Notifications",
+    route: '/notifications',
+    label: 'Notifications',
     icon: <Heart />,
-    modalName: "notiSheet",
+    modalName: 'notiSheet',
   },
-  { route: "/profile", label: "Profile", icon: <User2 /> },
-];
+  { route: '/profile', label: 'Profile', icon: <User2 /> },
+]
 
 const TabBar = ({ hideAppBar, show }: any) => {
   const variants = {
     visible: {
-      y: "0",
+      y: '0',
     },
     hidden: {
-      y: "100%",
+      y: '100%',
     },
-  };
+  }
 
-  const { setModalState } = useModalStateSlice();
+  const { setModalState } = useModalStateSlice()
 
   return (
     <motion.div
-      animate={hideAppBar ? "hidden" : "visible"}
+      animate={hideAppBar ? 'hidden' : 'visible'}
       variants={variants}
       transition={{ duration: 0.2 }}
       className={cn(
-        "p-2 bg-zinc-800 sm:hidden block absolute bottom-0 w-full",
+        'absolute bottom-0 block w-full bg-zinc-800 p-2 sm:hidden',
         { hidden: !show }
       )}
     >
@@ -62,8 +62,8 @@ const TabBar = ({ hideAppBar, show }: any) => {
                 to={route}
                 onClick={(e) => {
                   if (modal) {
-                    e.preventDefault();
-                    setModalState("openPostModal");
+                    e.preventDefault()
+                    setModalState('openPostModal')
                   }
                 }}
               >
@@ -74,7 +74,7 @@ const TabBar = ({ hideAppBar, show }: any) => {
         </ul>
       </div>
     </motion.div>
-  );
-};
+  )
+}
 
-export default TabBar;
+export default TabBar
